e342861412ee324c89761de197869bc8cc41f00c
rjtechforce-byte <rjtechforce@gmail.com>
2026-04-17 13:58:37 +0530

    Update generateCertificateImage.js

--- src/utils/generateCertificateImage.js
+++ src/utils/generateCertificateImage.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 registerFont(
- path.join(__dirname, "../assets/fonts/NotoSansDevanagari-Regular.ttf"),
+  path.join(__dirname, "../assets/fonts/NotoSansDevanagari-Regular.ttf"),
   { family: "UniversalFont" }
 );
 
