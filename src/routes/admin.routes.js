a93958ec8a469f7cbb738e78865a40c3faf8cba5
rjtechforce-byte <rjtechforce@gmail.com>
2026-04-24 20:32:36 +0530

    Add route for getting blocks in admin panel

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -5,7 +5,10 @@ const router = express.Router();
 
 const { getAllUsersData } = require("../controllers/admin.controller");
 const adminAuth = require("../middleware/adminAuth");
+const { getAllUsersData, getBlocks } = require("../controllers/admin.controller");
+
 
 router.get("/users", adminAuth, getAllUsersData);
+router.get("/blocks", adminAuth, getBlocks);
 
 module.exports = router;
