d7825230dd13c8e81157b8c3df85d31692c6596d
rjtechforce-byte <rjtechforce@gmail.com>
2026-04-24 19:22:22 +0530

    Update admin.controller.js

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -11,49 +11,56 @@ const getAllUsersData = async (req, res) => {
     const total = await User.countDocuments();
 
     const data = await User.aggregate([
-      {
-        $lookup: {
-          from: "examresults",
-          localField: "_id",
-          foreignField: "userId",
-          as: "examData"
-        }
-      },
-      {
-        $lookup: {
-          from: "certificates",
-          localField: "_id",
-          foreignField: "userId",
-          as: "certificateData"
-        }
-      },
-      {
-        $unwind: {
-          path: "$examData",
-          preserveNullAndEmptyArrays: true
-        }
-      },
-      {
-        $project: {
-          name: 1,
-          phone: 1,
-          email: 1,
-          block: 1,
-          address: 1,
-          score: "$examData.score",
-          result: "$examData.result",
-          attemptedExams: "$examAttempts",
-          certificateUrl: { $arrayElemAt: ["$certificateData.certificateUrl", 0] },
-          createdAt: 1
-        }
-      },
-      { $sort: { createdAt: -1 } },
+  {
+    $lookup: {
+      from: "examresults",
+      localField: "_id",
+      foreignField: "userId",
+      as: "examData"
+    }
+  },
+  {
+    $lookup: {
+      from: "certificates",
+      localField: "_id",
+      foreignField: "userId",
+      as: "certificateData"
+    }
+  },
 
-      { $skip: skip },     
-      { $limit: limit }    
-    ]);
+  // 🔥 latest exam only
+  {
+    $addFields: {
+      examData: { $arrayElemAt: ["$examData", -1] }
+    }
+  },
 
-    res.json({
+  {
+    $project: {
+      _id: 1,
+      name: 1,
+      phone: 1,
+      email: 1,
+      block: 1,
+      address: 1,
+
+      score: "$examData.score",
+      result: "$examData.result",
+
+      attemptedExams: "$examAttempts",
+
+      certificateUrl: {
+        $arrayElemAt: ["$certificateData.certificateUrl", 0]
+      },
+
+      createdAt: 1
+    }
+  },
+
+  { $sort: { createdAt: -1 } },
+  { $skip: skip },
+  { $limit: limit }
+]);
       data,
       total,
       page,
