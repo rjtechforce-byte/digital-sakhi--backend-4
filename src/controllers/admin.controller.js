854939fa8d6f8cb862a5d14f3c369b20fb7687b9
rjtechforce-byte <rjtechforce@gmail.com>
2026-04-24 16:13:20 +0530

    Create admin.controller.js

---
+++ src/controllers/admin.controller.js
@@ -0,0 +1,51 @@
+const User = require("../modals/user.modal");
+
+const getAllUsersData = async (req, res) => {
+  try {
+    const data = await User.aggregate([
+      {
+        $lookup: {
+          from: "examresults",
+          localField: "_id",
+          foreignField: "userId",
+          as: "examData"
+        }
+      },
+      {
+        $lookup: {
+          from: "certificates",
+          localField: "_id",
+          foreignField: "userId",
+          as: "certificateData"
+        }
+      },
+      {
+        $unwind: {
+          path: "$examData",
+          preserveNullAndEmptyArrays: true
+        }
+      },
+      {
+        $project: {
+          name: 1,
+          phone: 1,
+          email: 1,
+          block: 1,
+          address: 1,
+          score: "$examData.score",
+          result: "$examData.result",
+          certificateUrl: { $arrayElemAt: ["$certificateData.certificateUrl", 0] },
+          createdAt: 1
+        }
+      },
+      { $sort: { createdAt: -1 } }
+    ]);
+
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error fetching data" });
+  }
+};
+
+module.exports = { getAllUsersData };
