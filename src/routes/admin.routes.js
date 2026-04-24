a9a0d0ae89b63e9bfda28c74a3ad8f1aaafe0193
rjtechforce-byte <rjtechforce@gmail.com>
2026-04-24 20:47:25 +0530

    Update admin.routes.js

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const { getAllUsersData } = require("../controllers/admin.controller");
 const adminAuth = require("../middleware/adminAuth");
-const { getAllUsersData, getBlocks } = require("../controllers/admin.controller");
+const { getBlocks } = require("../controllers/admin.controller");
 
 
 router.get("/users", adminAuth, getAllUsersData);
