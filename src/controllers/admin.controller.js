519c0a89f2268993db7298c3b03cce5a9cb64d43
rjtechforce-byte <rjtechforce@gmail.com>
2026-04-24 18:23:45 +0530

    Update admin.controller.js

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -34,6 +34,7 @@ const getAllUsersData = async (req, res) => {
           address: 1,
           score: "$examData.score",
           result: "$examData.result",
+          attemptedExams: "$examData.attemptedExams",
           certificateUrl: { $arrayElemAt: ["$certificateData.certificateUrl", 0] },
           createdAt: 1
         }
